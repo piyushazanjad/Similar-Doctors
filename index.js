e98f200228bf9fb2870205e7f481b001a89d5325
piyushazanjad <piyusha_zanjad@csu.fullerton.edu>
2017-07-22 22:51:59 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ var doctorApp = angular.module('doctorApp', ['angular.filter', 'ngSanitize']);
 // Define the `DoctorController` controller on the `doctorApp` module
 doctorApp.controller('DoctorController', function DoctorController($scope, $filter) {
     $scope.areas = $filter('filter')(doctors, function (doctor) {
-        return doctor.area;//doctor
+        return doctor.area;
     });
     $scope.myVar = true;
     $scope.myDocs = true;
@@ -48,15 +48,12 @@ doctorApp.controller('DoctorController', function DoctorController($scope, $filt
     $scope.changeArea = function (obj) {
         ar = $scope.ar.area;
         var specs = new Set();
-
-        console.log(ar);
         for (var i = 0; i < doctors.length; i++) {
             var x = doctors[i].area;
             if (x == ar) {
                 specs.add(doctors[i].speciality);
             }
         }
-        console.log(specs);
         let specs_arr = Array.from(specs);
         $scope.specs = specs_arr;
     };
@@ -67,28 +64,25 @@ doctorApp.controller('DoctorController', function DoctorController($scope, $filt
 
     var final_list = [];
     var final_list_names = [];
-
-         $scope.changeDoctors = function(obj){
-            final_list = [];
-            final_list_names = [];
-            for(var i=0;i<doctors.length;i++){
+    
+    $scope.changeDoctors = function (obj) {
+        final_list = [];
+        final_list_names = [];
+        for (var i = 0; i < doctors.length; i++) {
             var ar = $scope.ar.area;
             var spec = $scope.sp;
-            if(doctors[i].area==ar && doctors[i].speciality==spec){
-              final_list.push(doctors[i]);
-              final_list_names.push(doctors[i].name);
+            if (doctors[i].area == ar && doctors[i].speciality == spec) {
+                final_list.push(doctors[i]);
+                final_list_names.push(doctors[i].name);
             }
-         } 
-         //$scope.myVar = false;
-          //$scope.mydoc = {name:n,area:ar,sp:spec,r:rating};
-          $scope.myDocs = false;
-          $scope.others = final_list;
         }
+        $scope.myDocs = false;
+        $scope.others = final_list;
+    }
 
 
     $scope.changeDocs = function (obj) {
         var rating = $scope.rating;
-        console.log(rating);
         final_list = [];
         final_list_names = [];
         for (var i = 0; i < doctors.length; i++) {
@@ -99,17 +93,16 @@ doctorApp.controller('DoctorController', function DoctorController($scope, $filt
                 final_list_names.push(doctors[i].name);
             }
         }
-        console.log(final_list);
         $scope.docs = final_list_names;
-        console.log(final_list_names);
     };
+
+
     $scope.showDocInfo = function (obj) {
-        //console.log(obj);
         var ar = $scope.ar.area;
         var spec = $scope.sp;
         var rating = null;
         for (var i = 0; i < final_list.length; i++) {
-            //var rating = final_list[i].rating;
+
             if (obj == final_list[i].name) {
                 rating = final_list[i].rating;
             }
